50ca9ee23209367dff09fbb28dde89253f66ecb1
TsubasaShimizu-Lamech <t.shimizu@lamech.jp>
2025-06-04 11:20:44 +0900

    favicon追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,10 @@ export const metadata: Metadata = {
   title: "Lamech (レメク)",
   description: "Lamech (レメク) - アプリ開発、システム開発、ITコンサルティング、DX支援、美容機器販売、治療機器販売を行う会社です。",
   keywords: "Lamech, レメク, アプリ開発, システム開発, ITコンサルティング, DX支援, 美容機器, 治療機器, 奈良県, 橿原市",
-  generator: 'v0.dev'
+  generator: 'v0.dev',
+  icons: {
+    icon: '/images/favicon.ico',
+  },
 }
 
 export default function RootLayout({
@@ -19,6 +22,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="scroll-smooth">
+      <head>
+        <link rel="icon" href="/images/favicon.ico" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
